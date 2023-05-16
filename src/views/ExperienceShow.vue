7aba048c6180dfb0b0cd1180158c3b109659632a
Dmitriy <dmitriy.f@devhouse.pro>
2023-05-17 00:06:54 +0400

    fix routeing

--- src/views/ExperienceShow.vue
+++ src/views/ExperienceShow.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="experience-show">
+    <section id="experience-show" v-if="experience">
         <h1>{{ experience.name }}</h1>
         <img :src="`/images/${experience.image}`" :alt="experience.name">
         <p>{{ experience.description }}</p>
@@ -8,30 +8,25 @@
 
 <script>
 export default {
-    data() {
-        return {
-            destination: ''
-        }
-    },
-    props: {
-        slug: {type: String, required: true},
-        experienceSlug: {type: String, required: true}
-    },
+  props: {
+    slug: {type: String, required: true},
+    experienceSlug: {type: String, required: true},
+  },
+  data() {
+      return {
+        experience: ''
+      }
+  },
+  async mounted() {
+    await this.initData();
+    this.$watch(() => this.$route.params, this.initData);
+  },
     methods: {
         async initData() {
-            const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`)
-            this.destination = await response.json()
+          const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`);
+          this.experience = (await response.json())?.experiences?.find(experience => experience.slug === this.experienceSlug);
         }
     },
-    async created() {
-        this.initData()
-        this.$watch(() => this.$route.params, this.initData)
-    },
-    computed: {
-        experience() {
-            return this.destination.experiences.find(experience => experience.slug === this.experienceSlug)
-        }
-    }
 }
-        
+
 </script>
