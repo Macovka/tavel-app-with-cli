a0d0dd861fb7db1a7f36c4b5db54fd500bf552d9
Macovka <dmakkoveeva6@gmail.com>
2023-05-18 13:04:42 +0400

    experience-hooks-changed

--- src/views/ExperienceShow.vue
+++ src/views/ExperienceShow.vue
@@ -1,41 +1,48 @@
 <template>
     <section id="experience-show" v-if="experience">
-        <h1>{{ experience.name }}</h1>
-        <img :src="`/images/${experience.image}`" :alt="experience.name">
-        <p>{{ experience.description }}</p>
+      <h1>{{ experience.name }}</h1>
+      <img :src="`/images/${experience.image}`" :alt="experience.name">
+      <p>{{ experience.description }}</p>
     </section>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     props: {
-        slug: {type: String, required: true},
-        experienceSlug: {type: String, required: true}
+      slug: { type: String, required: true },
+      experienceSlug: { type: String, required: true }
     },
     data() {
-        return {
-            experience: ''
-        }
-    },   
-    async created() {
-        await this.initData()
-        this.$watch(() => this.$route.params, this.initData)
+      return {
+        experience: ''
+      };
     },
-    async updated() {
+    async mounted() {
+      await this.initData();
+      this.$watch(() => this.$route.params, this.initData);
+      this.scrollToExperience();
+    },
+    methods: {
+      async initData() {
+        const response = await fetch(
+          `https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`
+        );
+        this.experience = (
+          await response.json()
+        )?.experiences?.find(
+          (experience) => experience.slug === this.experienceSlug
+        );
+      },
+      scrollToExperience() {
         if (this.$route.name === 'experience.show') {
-            await this.$nextTick();
+          this.$nextTick(() => {
             const experienceShowElement = document.getElementById('experience-show');
             if (experienceShowElement) {
-                experienceShowElement.scrollIntoView({ behavior: 'smooth' });
+              experienceShowElement.scrollIntoView({ behavior: 'smooth' });
             }
+          });
         }
-    }, 
-    methods: {
-        async initData() {
-            const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`)
-            this.experience = (await response.json())?.experiences?.find(experience => experience.slug === this.experienceSlug);
-        }
-    },
-}
-        
-</script>
+      }
+    }
+  };
+  </script>
